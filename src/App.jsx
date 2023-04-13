af57ff50b7b89f4cb7d8c87924ce6d8ae71ae7a2
Hermann <edgar.poe1994@gmail.com>
2023-04-13 20:51:45 +0300

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.scss'
+import Modalscreen from './Component/ModalScreen/Modalscreen'
 
 function App() {
-
   return (
     <div className="App">
-      
+      <Modalscreen />
     </div>
   )
 }
